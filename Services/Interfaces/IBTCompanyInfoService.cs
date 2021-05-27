440f0bd1ff983890a387188b6cda18a6f08b920b
John Flynn <johnbishopflynn@gmail.com>
2021-05-27 12:55:48 -0400

    Added new interface

---
+++ Services/Interfaces/IBTCompanyInfoService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BugTracker.Models;
+
+namespace BugTracker.Services.Interfaces
+{
+    interface IBTCompanyInfoService
+    {
+
+            Task<Company> GetCompanyInfoByIdAsync(int? companyId);
+
+            Task<List<BTUser>> GetAllMembersAsync(int companyId);
+
+            Task<List<BTUser>> GetAllProjectsAsync(int companyId);
+
+            Task<List<BTUser>> GetAllTicketsAsync(int companyId);
+
+            Task<List<BTUser>> GetMembersInRoleAsync(string roleName, int companyId);
+      
+
+    }
+}
