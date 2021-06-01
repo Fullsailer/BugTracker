59bc871e5b72d349aac12dc8939e179099ecac66
John Flynn <johnbishopflynn@gmail.com>
2021-06-01 12:52:50 -0400

    Added Ticket Service with Interface and Class

---
+++ Services/Interfaces/IBTTIcketService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BugTracker.Models;
+
+namespace BugTracker.Services.Interfaces
+{
+    public interface IBTTIcketService
+    {
+        Task AssignTicketAsync(int ticketId, string userId);
+        Task<BTUser> GetTicketDeveloperAsync(int ticketId);
+
+    }
+}
