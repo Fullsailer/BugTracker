d0fe3f32db32e0879c7a9b6bc3801d47dad2a48c
John Flynn <johnbishopflynn@gmail.com>
2021-06-07 13:21:44 -0400

    Added Dashboard View Model

---
+++ Models/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Models.ViewModels
+{
+    public class DashboardViewModel
+    {
+        public List<Project> Porjects { get; set; }
+
+        public List<Ticket> Tickets { get; set; }
+
+        public List<BTUser> Users { get; set; }
+    }
+}
